bd209895ef2538d81cec6c465686cf67b63a39fc
Charlie Jacobsen <charlie.jacobsen@utah.edu>
2014-06-20 16:17:33 -0600

    Driver code and interface for loading a blob in an lcd (untested).
    User code calls ioctl with LCD_RUN_BLOB ioctl number and
    lcd_blob_info (containing userspace address of blob and
    blob order) -- defined in public include/linux/lcd-domains.h.
    
    The blob must be N pages, and N must be a power of 2 (for
    easy driver code). blob_order = log2(N). The blob consists
    of machine instructions that are loaded in the lcd and
    executed. The machine instructions cannot access any memory,
    including the stack (for now, until gv paging is in place).
    
    -- Added lcd_arch_set_pc for setting the lcd's program counter.
    -- Added driver code in lcd-domains.c for handling the ioctl
       request, loading the blob from user space into a fresh lcd,
       and running the lcd (in a loop).

--- include/linux/lcd-domains.h
+++ include/linux/lcd-domains.h
@@ -2,7 +2,7 @@
 #define LCD_DOMAINS_H
 
 /*
- * lcd.h - public header for LCD support
+ * lcd-domains.h - public header for LCD support
  */
 
 #include <linux/types.h>
@@ -14,11 +14,17 @@
 /* FIXME: this must be reserved in miscdevice.h */
 #define LCD_MINOR       234
 
-#define LCD_LOAD_PV_KERNEL	 _IOR(LCD_MINOR, 0x01, struct lcd_pv_kernel_config)
-
 struct lcd_pv_kernel_config {
 	char  file[PATH_MAX];
 	__u64 length;
 } __attribute__((packed));
+
+struct lcd_blob_info {
+	unsigned char *blob;
+	unsigned int blob_order;
+} __attribute__((packed));
+
+#define LCD_LOAD_PV_KERNEL	 _IOR(LCD_MINOR, 0x01, struct lcd_pv_kernel_config)
+#define LCD_RUN_BLOB             _IOR(LCD_MINOR, 0x02, struct lcd_blob_info)
 
 #endif /* LCD_DOMAINS_H */
