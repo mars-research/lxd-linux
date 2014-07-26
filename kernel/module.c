8ce78bb023fd4994748bacd7962806990e45636e
Charlie Jacobsen <charlie.jacobsen@utah.edu>
2014-07-26 16:31:58 -0600

    Basic lcd module create, run, and destroy.
    This code is ugly, but it's working.
    
    Tested with basic module, and appears to be working
    properly. I will soon incorporate the patched
    modprobe into the kernel tree, and then this code
    will be usable by everyone.
    
    The ipc code is still unimplemented. The only
    hypercall handled is yield. Also note that other
    exit conditions (e.g. external interrupt) have not
    been fully tested.
    
    Overview:
    -- kernel code calls lcd_create_as_module with
       the module's name
    -- lcd_create_as_module loads the module using
       request_lcd_module (request_lcd_module calls
       the patched modprobe to load the module, and
       the patched modprobe calls back into the lcd
       driver via the ioctrl interface to load the
       module)
    -- lcd_create_as_module then finds the loaded
       module, spawns a kernel thread and passes off
       the module to it
    -- the kernel thread initializes the lcd and
       maps the module inside it, then suspends itself
    -- lcd_run_as_module wakes up the kernel thread
       and tells it to run
    -- lcd_delete_as_module stops the kernel thread
       and deletes the module from the host kernel
    
    File-by-file details:
    
    arch/x86/include/asm/lcd-domains-arch.h
    arch/x86/lcd-domains/lcd-domains-arch-tests.c
    arch/x86/lcd-domains/lcd-domains-arch.c
    -- lcd was not running in 64-bit mode, and my
       checks had one subtle bug
    -- fixed %cr3 load to properly load vmcs first
    -- fixed set program counter to use guest virtual
       rather than guest physical address
    
    include/linux/sched.h
    -- added struct lcd to task_struct
    
    include/linux/init_task.h
    -- lcd pointer set to null when task_struct is
       initialized
    
    include/linux/module.h
    kernel/module.c
    -- made init_module and delete_module system calls
       callable from kernel code
    -- available in module.h via do_sys_init_module and
       do_sys_delete_module
    -- simply moved the majority of the guts of the
       system calls into a non-system call, exported
       routine
    -- take an extra flag, for_lcd; when set, the init
       code skips over running (and deallocating) the
       module's init code, and the delete code skips
       over running the module exit
    -- system calls from user code set for_lcd = 0; this
       ensures existing code still works
    
    include/linux/kmod.h
    kernel/kmod.c
    kernel/sysctl.c
    -- changed __request_module to __do_request_module; takes
       one extra argument, for_lcd
    -- __request_module   ==>  __do_request_module with for_lcd = 0
    -- request_lcd_module ==>  __do_request_module with for_lcd = 1
    -- call_modprobe conditionally uses lcd_modprobe_path, the path
       to a patched modprobe accessible via sysfs
    
    include/lcd-domains/lcd-domains.h
    -- added lcd status enum; see source code doc
    -- three routines for creating/running/destroying
       lcd's that use modules; see source code doc
    
    include/uapi/linux/lcd-domains.h
    -- added interface defns for patched modprobe to call into
       lcd driver for module init; lcd driver loads
       module (via slightly refactored module.c code) on behalf
       of modprobe
    
    virt/lcd-domains/lcd-domains.c
    -- implementation of routines for modules inside lcd's
    -- implementation of module init / delete for lcd's
       (uses patched module.c code)
    
    virt/lcd-domains/Kconfig
    virt/lcd-domains/Makefile
    virt/lcd-domains/lcd-module-load-test.c
    virt/lcd-domains/lcd-tests.c
    -- added test module for lcd module code
    -- test runs automatically when lcd module is inserted

--- kernel/module.c
+++ kernel/module.c
@@ -820,19 +820,15 @@ static void wait_for_zero_refcount(struct module *mod)
 	mutex_lock(&module_mutex);
 }
 
-SYSCALL_DEFINE2(delete_module, const char __user *, name_user,
-		unsigned int, flags)
+int do_sys_delete_module(const char *name, unsigned int flags,
+			int for_lcd)
 {
 	struct module *mod;
-	char name[MODULE_NAME_LEN];
+
 	int ret, forced = 0;
 
 	if (!capable(CAP_SYS_MODULE) || modules_disabled)
 		return -EPERM;
-
-	if (strncpy_from_user(name, name_user, MODULE_NAME_LEN-1) < 0)
-		return -EFAULT;
-	name[MODULE_NAME_LEN-1] = '\0';
 
 	if (mutex_lock_interruptible(&module_mutex) != 0)
 		return -EINTR;
@@ -881,8 +877,9 @@ SYSCALL_DEFINE2(delete_module, const char __user *, name_user,
 		wait_for_zero_refcount(mod);
 
 	mutex_unlock(&module_mutex);
+
 	/* Final destruction now no one is using it. */
-	if (mod->exit != NULL)
+	if (mod->exit != NULL && !for_lcd)
 		mod->exit();
 	blocking_notifier_call_chain(&module_notify_list,
 				     MODULE_STATE_GOING, mod);
@@ -896,6 +893,19 @@ SYSCALL_DEFINE2(delete_module, const char __user *, name_user,
 out:
 	mutex_unlock(&module_mutex);
 	return ret;
+}
+EXPORT_SYMBOL(do_sys_delete_module);
+
+SYSCALL_DEFINE2(delete_module, const char __user *, name_user,
+		unsigned int, flags)
+{
+	char name[MODULE_NAME_LEN];
+
+	if (strncpy_from_user(name, name_user, MODULE_NAME_LEN-1) < 0)
+		return -EFAULT;
+	name[MODULE_NAME_LEN-1] = '\0';
+
+	return do_sys_delete_module(name, flags, 0);
 }
 
 static inline void print_unload_info(struct seq_file *m, struct module *mod)
@@ -3031,7 +3041,7 @@ static void do_mod_ctors(struct module *mod)
 }
 
 /* This is where the real work happens */
-static int do_init_module(struct module *mod)
+static int do_init_module(struct module *mod, int for_lcd)
 {
 	int ret = 0;
 
@@ -3058,7 +3068,7 @@ static int do_init_module(struct module *mod)
 
 	do_mod_ctors(mod);
 	/* Start the module */
-	if (mod->init != NULL)
+	if (mod->init != NULL && !for_lcd)
 		ret = do_one_initcall(mod->init);
 	if (ret < 0) {
 		/* Init routine failed: abort.  Try to protect us from
@@ -3115,12 +3125,19 @@ static int do_init_module(struct module *mod)
 	mod->symtab = mod->core_symtab;
 	mod->strtab = mod->core_strtab;
 #endif
-	unset_module_init_ro_nx(mod);
-	module_free(mod, mod->module_init);
-	mod->module_init = NULL;
-	mod->init_size = 0;
-	mod->init_ro_size = 0;
-	mod->init_text_size = 0;
+	if (!for_lcd) {
+		/* 
+		 * Only free init code if we're not going to run in
+		 * an lcd. If we will run in an lcd, init code will
+		 * be deallocated via free_module in do_sys_delete_module.
+		 */
+		unset_module_init_ro_nx(mod);
+		module_free(mod, mod->module_init);
+		mod->module_init = NULL;
+		mod->init_size = 0;
+		mod->init_ro_size = 0;
+		mod->init_text_size = 0;
+	}
 	mutex_unlock(&module_mutex);
 	wake_up_all(&module_wq);
 
@@ -3198,7 +3215,7 @@ out:
 /* Allocate and load the module: note that size of section 0 is always
    zero, and we rely on this for optional sections. */
 static int load_module(struct load_info *info, const char __user *uargs,
-		       int flags)
+		int flags, int for_lcd)
 {
 	struct module *mod;
 	long err;
@@ -3301,7 +3318,7 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	/* Done! */
 	trace_module_load(mod);
 
-	return do_init_module(mod);
+	return do_init_module(mod, for_lcd);
 
  bug_cleanup:
 	/* module_bug_cleanup needs module_mutex protection */
@@ -3331,8 +3348,8 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	return err;
 }
 
-SYSCALL_DEFINE3(init_module, void __user *, umod,
-		unsigned long, len, const char __user *, uargs)
+int do_sys_init_module(void __user *umod, unsigned long len,
+		const char __user *uargs, int for_lcd)
 {
 	int err;
 	struct load_info info = { };
@@ -3348,7 +3365,14 @@ SYSCALL_DEFINE3(init_module, void __user *, umod,
 	if (err)
 		return err;
 
-	return load_module(&info, uargs, 0);
+	return load_module(&info, uargs, 0, for_lcd);
+}
+EXPORT_SYMBOL(do_sys_init_module);
+
+SYSCALL_DEFINE3(init_module, void __user *, umod,
+		unsigned long, len, const char __user *, uargs)
+{
+	return do_sys_init_module(umod, len, uargs, 0);
 }
 
 SYSCALL_DEFINE3(finit_module, int, fd, const char __user *, uargs, int, flags)
@@ -3370,7 +3394,7 @@ SYSCALL_DEFINE3(finit_module, int, fd, const char __user *, uargs, int, flags)
 	if (err)
 		return err;
 
-	return load_module(&info, uargs, flags);
+	return load_module(&info, uargs, flags, 0);
 }
 
 static inline int within(unsigned long addr, void *start, unsigned long size)
