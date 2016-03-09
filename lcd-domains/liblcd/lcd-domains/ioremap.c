437219f9ffd002d83cf1098855ef7740884d910f
Abhiram Balasubramanian <abhiram@cs.utah.edu>
2016-03-09 14:57:43 -0700

    Refactor ioremap implementation
    - adhere to Charlie's recent code modifications
    
    Signed-off-by: Abhiram Balasubramanian <abhiram@cs.utah.edu>

--- lcd-domains/liblcd/lcd-domains/ioremap.c
+++ lcd-domains/liblcd/lcd-domains/ioremap.c
@@ -1,65 +1,155 @@
+/*
+ * ioremap - code for mapping device memory to LCD
+ */ 
 
-/* Temporary staging for Abhi's ioremap */
+#include <linux/slab.h>
+#include <liblcd/allocator.h>
+#include <liblcd/mem.h>
+#include <asm/lcd_domains/liblcd.h>
+#include <lcd_domains/liblcd.h>
 
-#define LCD_IOREMAP_GPA_BASE __gpa(0xc9UL << 40)
-#define LCD_IOREMAP_GPA_SIZE (1UL << 24) /* 16 MB set aside for ioremap */
-#define LCD_IOREMAP_GVA __gva(gpa_val(LCD_IOREMAP_GPA))
+struct lcd_page_allocator *ioremap_allocator;
 
-/* Bit-map to track free space in ioremap'ble address space */
-#define LCD_IOREMAP_BMAP_SIZE (LCD_IOREMAP_GPA_SIZE >> PAGE_SHIFT)
-static DECLARE_BITMAP(ioremap_gpa_bmap, LCD_IOREMAP_BMAP_SIZE);
-static cptr_t ioremap_gpa_phys2cptr[LCD_FREE_MEM_BMAP_SIZE];
-
-int gp_ioremap(cptr_t phys_addr, unsigned long size, gpa_t *base) 
+void *lcd_ioremap(unsigned long phys_addr, unsigned int size)
 {
-	
-	unsigned int slots = 0;
-	unsigned int index = 0;
-	int ret = 0;
+	return (void *)(gva_val(lcd_gpa2gva(__gpa(phys_addr))));
+}
 
-	slots = size >> PAGE_SHIFT;
-	index = find_first_zero_bits(ioremap_gpa_bmap, LCD_IOREMAP_BMAP_SIZE, slots);
-	if(index >= LCD_IOREMAP_BMAP_SIZE) {
-		lcd_printk("gpa_ioremap: exhausted memory space in GPA \n");
-		return -ENOMEM;		
+static inline gpa_t ioremap_page_block_to_addr(struct lcd_page_block *pb)
+{
+        return gpa_add(LCD_IOREMAP_GP_ADDR,
+                lcd_page_block_to_offset(ioremap_allocator, pb));
+}
+
+static inline struct lcd_page_block *ioremap_addr_to_page_block(gpa_t addr)
+{
+        return lcd_offset_to_page_block(
+                ioremap_allocator,
+                gpa_val(addr) - gpa_val(LCD_IOREMAP_GP_ADDR));
+}
+
+int _lcd_ioremap(cptr_t phys_addr, unsigned int size, gpa_t *base) {
+	
+	unsigned int order = 0;
+	struct lcd_page_block *pb = NULL;
+	int ret = 0;
+	gpa_t addr;
+
+	order = ilog2(size >> PAGE_SHIFT);	
+	if(order < LCD_IOREMAP_MIN_ORDER) {
+		order = LCD_IOREMAP_MIN_ORDER;
 	}
- 	
-	/* Size required for mapping is figured out by the microkernel as capabilities 
- 	 * are associated with their size */
-	*base = gpa_add(LCD_IOREMAP_GPA_BASE, index << PAGE_SHIFT);
-	ret = lcd_page_map(phys_addr, *base);
+	
+	pb = lcd_page_allocator_alloc(ioremap_allocator, order);
+	if(!pb) {
+                LIBLCD_ERR("failed to get free ioremap region");
+                ret = -ENOMEM;
+		goto exit;
+	}
+	
+	addr = ioremap_page_block_to_addr(pb);
+	
+	ret = lcd_syscall_mmap(phys_addr, addr);
         if (ret) {
-                lcd_printk("gpa_ioremap: cannot map physical address to GPA \n");
-                return ret;
+                LIBLCD_ERR("low level mmap failed");
+                goto exit_mmap;
         }
 
-	set_bits(index, ioremap_gpa_bmap, slots);
-	ioremap_gpa_phys2cptr[index] = phys_addr;
+        ret = __liblcd_mem_itree_insert(base, (1UL << (PAGE_SHIFT + order)),
+                                        phys_addr);
+        if (ret) {
+                LIBLCD_ERR("error inserting into mem itree");
+                goto exit_itree_insert;
+        }
+
+	return ret;
+
+exit_itree_insert:
+	lcd_syscall_munmap(phys_addr);
+
+exit_mmap:
+	lcd_page_allocator_free(ioremap_allocator, pb, order);
+	
+exit:
 	return ret;
 }
 
-int gp_iounmap(gpa_t phys_addr, unsigned long size)
+void _lcd_iounmap(gpa_t phys_addr, unsigned long size)
 {
-	unsigned int slots = 0;
-	unsigned index = 0;
-	int ret = 0;
+        int ret;
+        struct lcd_resource_node *n; 
+	unsigned int order = 0;
 
-	slots = size >> PAGE_SHIFT;
-	index = (gpa_val(phys_addr) - gpa_val(LCD_IOREMAP_GPA_BASE)) >> PAGE_SHIFT;
-	
-	if(!(gpa_val(phys_addr) >= gpa_val(LCD_IOREMAP_GPA_BASE) &&
-		gpa_val(gpa_add(phys_addr, index << PAGE_SHIFT)) < 
-		gpa_val(gpa_add(LCD_IOREMAP_GPA_BASE, LCD_IOREMAP_GPA_SIZE)))) {
+	order = ilog2(size >> PAGE_SHIFT);
 
-		lcd_printk("gp_iounmap: Trying to unmap invalid region of memory \n");
-		return -EFAULT;
-	}	
-	
-	ret = lcd_page_unmap(ioremap_gpa_phys2cptr[index], phys_addr);
-	if(ret) {
-		lcd_printk("gp_iounmap: unmap failed \n");
-	}
-	clear_bits(index, ioremap_gpa_bmap, slots);	
-	ioremap_gpa_phys2cptr[index] = LCD_CPTR_NULL;
-	return ret; 
+        if (order < LCD_RAM_MAP_MIN_ORDER)
+                order = LCD_RAM_MAP_MIN_ORDER;
+
+        ret = lcd_phys_to_resource_node(phys_addr, &n);
+        if (ret) { 
+                LIBLCD_ERR("error looking up resource node");
+                return;
+        }
+
+        lcd_page_allocator_free(
+                ioremapmap_allocator,
+                ioremap_addr_to_page_block(phys_addr),
+                order);
+
+        __liblcd_mem_itree_delete(n);
+        
+	lcd_syscall_munmap(phys_addr);
 }
+
+static int
+alloc_ioremap_metadata_memory(const struct lcd_page_allocator_cbs *cbs,
+                        unsigned int alloc_order,
+                        unsigned long metadata_sz,
+                        void **metadata_addr)
+{
+        int ret = 0;
+        
+	*metadata_addr = kmalloc(metadata_sz, GFP_KERNEL);
+        if (!*metadata_addr) {
+                LIBLCD_ERR("error getting metadata mem for ioremap");
+                ret = -ENOMEM;
+                goto exit;
+        }
+
+        return ret;
+}
+
+static void
+ram_free_unmap_metadata_memory(const struct lcd_page_allocator_cbs *cbs,
+                        void *metadata_addr,
+                        unsigned long metadata_sz,
+                        unsigned int alloc_order)
+{
+        kfree(metadata_addr);
+}
+
+struct lcd_page_allocator_cbs ioremap_page_allocator_cbs = {
+        .alloc_map_metadata_memory = alloc_ioremap_metadata_memory,
+        .free_unmap_metadata_memory = free_ioremap_metadata_memory,
+};
+
+int __liblcd__ioremap_init(void)
+{
+        int ret;
+        ret = lcd_page_allocator_create(LCD_IOREMAP_NR_PAGES_ORDER,
+                                        LCD_IOREMAP_MIN_ORDER,
+                                        LCD_IOREMAP_MAX_ORDER,
+                                        &ioremap_page_allocator_cbs,
+                                        0, /* don't embed metadata */
+                                        &ioremap_allocator);
+        if (ret) {
+                LIBLCD_ERR("failed to initialize RAM map allocator");
+                goto exit;
+        }
+
+        return 0;
+
+exit:
+        return ret;
+}
+
