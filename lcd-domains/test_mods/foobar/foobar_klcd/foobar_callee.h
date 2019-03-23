a6b88a3c8c110878706e2010cdbcf2234437c616
Aftab Hussain <aftab.hussain46@gmail.com>
2019-03-22 17:24:01 -0700

    foobar: Remove fn re-declaration in callee header

--- lcd-domains/test_mods/foobar/foobar_klcd/foobar_callee.h
+++ lcd-domains/test_mods/foobar/foobar_klcd/foobar_callee.h
@@ -19,10 +19,6 @@ int free_foobardev_callee(struct fipc_message *_request,
 		struct thc_channel *_channel,
 		struct glue_cspace *cspace,
 		struct cptr sync_ep);
-int free_foobardev_callee(struct fipc_message *_request,
-		struct thc_channel *_channel,
-		struct glue_cspace *cspace,
-		struct cptr sync_ep);
 void dispatch_sync_loop(void);
 int dispatch_async_loop(struct thc_channel *_channel,
 		struct fipc_message *message,
